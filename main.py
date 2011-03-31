478c78334fde98952083b3d76dc59cb64bff007e
Dheeraj Sayala <dheerosaur@gmail.com>
2011-03-31 17:01:45 +0530

    Handlers, initial version

--- main.py
+++ main.py
@@ -73,6 +73,80 @@ class BaseRequestHandler(webapp.RequestHandler):
         self.response.out.write(template.render(path, values, debug=_DEBUG))
 
 # handlers
+class TimeLineHandler(BaseRequestHandler):
+    """If the user is logged in, gets tweets of whom the user follows. Otherwise, shows the welcome
+    page asking him to login.
+    """
+    @login_required
+    def get(self):
+        bird = Bird.get_current_bird()
+        if not bird.username:
+            self.redirect('/register')
+        following = bird.following_set.all()
+        tweets = Tweet.gql("WHERE author in :following", following=following).order(
+                "-published")
+        self.generate("timeline.html", {
+            'tweets': tweets,
+            'bird': bird })
+
+class RegisterHandler(BaseRequestHandler):
+    """Asks the user to select a username/handle.
+    """
+    @login_required
+    def get(self):
+        bird = Bird.get_current_bird()
+        self.generate("register.html", {
+            'bird': bird })
+
+    @login_required
+    def post(self):
+        bird = Bird.get_current_bird()
+        bird.username = cgi.escape(self.request.get('username'))
+        bird.about = cgi.escape(self.request.get('about'))
+        bird.put()
+
+class UserTimeLineHandler(BaseRequestHandler):
+    """Gets the timeline of a user.
+    """
+    def get(self, username):
+        bird = Bird.get_current_by_username(username)
+        if not bird:
+            self.error(403)
+            return
+        tweets = Tweet.gql("WHERE username = :username").fetch(20)
+        self.generate("timelin.html", {
+            'tweets': tweets,
+            'bird': bird })
+
+class TweetHandler(BaseRequestHandler):
+    """Posts a tweet."""
+    def post(self):
+        bird = Bird.get_current_bird()
+        tweet = Tweet(message=self.request.get('message'),
+                author=bird,
+                username=bird.username)
+        tweet.put()
+
+class FollowHandler(BaseRequestHandler):
+    """Adds a relation."""
+    def post(self, username):
+        follower = Bird.get_current_bird()
+        follower.no_following += 1
+        following = Bird.get_current_by_username(username)
+        following.no_followers += 1
+        follower.put()
+        following.put()
+        Relation(follower=follower, following=following).put()
+        self.redirect(self.request.get('next'))
+
+class PublicTimeLineHandler(BaseRequestHandler):
+    """Shows public time line on the home page"""
+    def get(self):
+        public_tweets = Tweet.all().order('-published').fetch(20)
+        self.response.out.write(template.render(
+            'public.html',
+            { 'publictweets' : public_tweets }))
+
 
 _URLS = (('/', TimeLineHandler),
          ('/register', RegisterHandler),
