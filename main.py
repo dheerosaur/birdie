8678921983f37339dcb82cf8a09acb8b9b3145d1
Dheeraj Sayala <dheerosaur@gmail.com>
2011-03-31 16:13:30 +0530

    Models and their methods defined

--- main.py
+++ main.py
@@ -15,6 +15,45 @@ from google.appengine.ext.webapp.util import login_required
 _DEBUG = True
 
 # models
+class Bird(db.Model):
+    """A registered user.
+
+    A username is generated after checking the availability. Currently logs in using Google
+    account."""
+    account = db.UserProperty(auto_current_user_add=True)
+    username = db.StringProperty()
+    about = db.StringProperty()
+    joined = db.DateTimeProperty(auto_now_add=True)
+    protected = db.BooleanProperty(default=False)
+    no_tweets = db.IntegerProperty()
+    no_followers = db.IntegerProperty()
+    no_following = db.IntegerProperty()
+
+    @staticmethod
+    def get_current_bird(self):
+        user = users.get_current_user()
+        bird = Bird.gql("WHERE account = :user", user=user)
+        return bird
+
+    @staticmethod
+    def get_current_by_username(self, username):
+        bird = Bird.gql("WHERE username = :username", username=username)
+        return bird
+
+class Relation(db.Model):
+    """Who follows Whom.
+
+    A relation is added whenever a user follows another user"""
+    follower = db.ReferenceProperty(Bird)
+    following = db.ReferenceProperty(Bird)
+
+class Tweet(db.Model):
+    """A tweet by a bird.
+    """
+    message = db.StringProperty(required=True)
+    published = db.DateTimeProperty(auto_now=True)
+    author = db.ReferenceProperty(Bird)
+    username = db.StringProperty(required=True)
 
 # generate
 
